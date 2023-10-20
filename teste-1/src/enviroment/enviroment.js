c96987f4933d11ccbd17eea6ee150e8e4f2763c3
Hamfel <ha1000tonfelipe@hotmail.com>
2023-10-20 16:02:00 -0300

    iniciando o projeto

---
+++ teste-1/src/enviroment/enviroment.js
@@ -0,0 +1,5 @@
+/*forma de comunicar uma URl através de API*/
+/*criando variáveis e jogando ela para todo o código*/
+export const ENV_PORT = 3000
+export const ENV_HOST = 'localhost'
+export const ENV_BASE_URL = "http://" + ENV_HOST + ":"+ ENV_PORT
